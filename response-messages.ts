84a05b3bfafb1bb3753c7a2a05ccb78a56afdc1c
koibhinheen <koibhinheen@gmail.com>
2023-03-23 13:30:57 +0500

    centralized most response messages

---
+++ response-messages.ts
@@ -0,0 +1,29 @@
+export const auth = {
+    error: {
+        loginRequired: "Please login to use this resource.",
+        invalidAuthTokenProvided: "The provided auth token is invalid.",
+        userDoesNotExist: "A user with the provided credentials does not exist."
+    }
+}
+
+import { userNameLength, passwordLength } from "./routes/user"
+export const signUp = {
+    error: {
+        // Username errors
+        userNameLength: `Username has to be at least ${userNameLength} characters long.`,
+
+        // Password errors
+        passwordLength: `Password has to be at least ${passwordLength} characters long.`,
+        specialCharsInPassword: "Password must contain atleast one special character.",
+        upperCharsInPassword: "Password must contain atleast one uppercase character.",
+        lowerCharsInPassword: "Password must contain atleast one lowercase character.",
+        digitsInPassword: "Password must contain atleast one digit.",
+
+        // Email errors
+        badEmail: "Please enter a valid email.",
+        emailAlreadyUsed: "The provided email is already in use",
+    },
+    success: {
+        userCreated: "User created successfully.",
+    },
+}
